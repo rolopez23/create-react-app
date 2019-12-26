c5ae0213796c42004e6bbc0fc54fec850ded83ce
Hack Reactor Student <student@hackreactor.com>
2019-12-26 13:20:06 -0800

    Completes first sections of course

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
+//App is a class based component
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//person is a functional componentent
+const person = () => {
+  return <p>I'm a Person!</p>
+};
+
+export default person;
