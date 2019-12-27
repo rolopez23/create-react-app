634dc882ce9235e803792653088700283556ef88
Hack Reactor Student <student@hackreactor.com>
2019-12-26 20:19:53 -0800

    Refactors to use hooks

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
-
+//Hooks don't merge but replace
 //App is a class based component
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] =  useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value'
-  }
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // console.log('Was clicked!')
-    this.setState({persons: [
+    setPersonsState({
+      persons: [
       {name: 'Maximilian', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 27}
-    ]})
-  }
+    ]});
+  };
 
-  render() {
+  console.log(personsState);
+
+  // render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person  name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person  name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person  name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+        <Person  name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
 }
 
-export default App;
+
+export default app;
